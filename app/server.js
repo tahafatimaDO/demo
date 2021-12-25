69c1761f72ba368eea0c53ffd64c23ac79625153
tahafatimaDO <82136225+tahafatimaDO@users.noreply.github.com>
2021-12-25 05:48:22 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -123,6 +123,6 @@ io.on('connection', socket => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
